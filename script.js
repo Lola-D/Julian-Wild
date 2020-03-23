c27edcd65dd7c3b26d46ebd0f02d11a30d501951
Lola <lola.donval@gmail.com>
2020-03-23 19:23:43 +0100

    add js changing border color title-contry gallery

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function onClick(element) {
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
 }
+
+var colors = ["#F8C364", "#ef7858", "#9cbdbe", "#2B7377"]
+var currentColor = 0
+var lis = document.querySelectorAll(".text h2")
+
+function changeColor() {
+    --currentColor
+    if (currentColor < 0) currentColor = colors.length - 1
+    for (var i = 0; i < lis.length; i++) {
+        lis[i].style.borderLeftColor = colors[(currentColor + i) % colors.length]
+    }
+}
+setInterval(changeColor, 1000)
